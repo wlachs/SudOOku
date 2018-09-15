1a97fbd8edf81ba4663a5de8bba75570d9318af4
László <laci.dj@gmail.com>
2018-09-15 22:06:27 +0200

    Matrix header basics

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -5,9 +5,26 @@
 #ifndef SUDOOKU_MATRIX_H
 #define SUDOOKU_MATRIX_H
 
+#include <map>
+#include "field.h"
 
 class Matrix {
+private:
+    std::map<std::pair<unsigned short int, unsigned short int>, Field> fields;
+public:
+    Matrix(unsigned short int, std::map<std::pair<unsigned short int, unsigned short int>, Field> &);
 
+    Matrix clone();
+
+    std::vector<Field const &> getListOfNontrivialFields();
+
+    unsigned short int getDimension();
+
+    void fixValue(Field const &, unsigned short int);
+
+    void removeValue(Field const &, unsigned short int);
+
+    void validateMatrix();
 };
 
 
