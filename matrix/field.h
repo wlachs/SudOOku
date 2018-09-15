6e25b139682e2342c5784e24c8a8460475b2f3c0
László <laci.dj@gmail.com>
2018-09-15 21:45:40 +0200

    Field header basics

--- matrix/field.h
+++ matrix/field.h
@@ -6,8 +6,23 @@
 #define SUDOOKU_FIELD_H
 
 
-class Field {
+#include <set>
+#include <iostream>
+#include "matrix.h"
 
+class Field {
+private:
+    Matrix const &matrix;
+    std::set<unsigned short int> possibleValues;
+
+public:
+    Field(Matrix const &matrix, unsigned short int dimension);
+
+    std::set<unsigned short int> &getPossibleValues();
+
+    void fixValue(unsigned short int value);
+
+    void removeValue(unsigned short int value);
 };
 
 
