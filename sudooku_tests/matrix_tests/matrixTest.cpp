74dd6270b363f2ade0ef98f25535476d3fc1e1e7
László <laci.dj@gmail.com>
2018-10-22 00:31:42 +0200

    matrix tests

---
+++ sudooku_tests/matrix_tests/matrixTest.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Borbély László on 2018. 10. 21..
+//
+
+#include <gtest/gtest.h>
+#include <matrix/matrix.h>
+#include <matrix/matrixReader.h>
+
+class MatrixTests : public ::testing::Test {
+protected:
+    Matrix m1;
+
+    void SetUp() override {
+        m1 = (Matrix) MatrixReader{"small4.mat"};
+    }
+};
+
+TEST_F(MatrixTests, getDimension_test) {
+    ASSERT_EQ(4, m1.getDimension());
+}
+
+TEST_F(MatrixTests, const_operator_test) {
+    Matrix const &m_const = m1;
+    Field const &field = m_const[{1, 1}];
+    auto const &possibleValues = field.getPossibleValues();
+
+    ASSERT_EQ(4, possibleValues.size());
+    for (unsigned short int i = 0; i < 4; ++i) {
+        EXPECT_TRUE(field.contains(i + 1));
+    }
+}
+
+TEST_F(MatrixTests, operator_test) {
+    Field const &field = m1[{1, 1}];
+    auto const &possibleValues = field.getPossibleValues();
+
+    ASSERT_EQ(4, possibleValues.size());
+    for (unsigned short int i = 0; i < 4; ++i) {
+        EXPECT_TRUE(field.contains(i + 1));
+    }
+}
+
+TEST_F(MatrixTests, fork_test) {
+    auto m2 = m1.forkFirstReturnSecond();
+    auto forkedList1 = m1[{1, 1}].getPossibleValues();
+    auto forkedList2 = m2[{1, 1}].getPossibleValues();
+
+    EXPECT_EQ(1, forkedList1.size());
+    EXPECT_EQ(3, forkedList2.size());
+}
+
+TEST_F(MatrixTests, print_test) {
+    std::cout << m1;
+}
