b0ea4f60a520df4786c54de2e5cbd840bb1fae90
László <laci.dj@gmail.com>
2018-10-24 21:07:44 +0200

    Controller tests

--- sudooku_tests/controller_tests/controllerTest.cpp
+++ sudooku_tests/controller_tests/controllerTest.cpp
@@ -6,11 +6,15 @@
 #include <gmock/gmock.h>
 #include <sudookuController.h>
 #include <controller_tests/helper_classes/mockInputHandler.h>
+#include <controller_tests/helper_classes/mockOutputHandler.h>
+
+using ::testing::AtLeast;
 
 class ControllerTests : public ::testing::Test {
 protected:
     SudookuController *sudookuController = nullptr;
     MockInputHandler mockInputHandler{};
+    MockOutputHandler mockOutputHandler;
 
     void SetUp() override {
         sudookuController = new SudookuController{&mockInputHandler, nullptr};
@@ -22,5 +26,21 @@ protected:
 };
 
 TEST_F(ControllerTests, input_called_only_once_test) {
-    EXPECT_CALL(mockInputHandler, readInput()).Times(1);
+    EXPECT_CALL(mockInputHandler, readInput())
+            .Times(1);
+}
+
+TEST_F(ControllerTests, output_handler_start_test) {
+    EXPECT_CALL(mockOutputHandler, notifyEvent(SudookuEvent::RUN_START, testing::_))
+            .Times(1);
+}
+
+TEST_F(ControllerTests, output_handler_solution_test) {
+    EXPECT_CALL(mockOutputHandler, notifyEvent(SudookuEvent::SOLUTION, testing::_))
+            .Times(AtLeast(1));
+}
+
+TEST_F(ControllerTests, output_handler_end_test) {
+    EXPECT_CALL(mockOutputHandler, notifyEvent(SudookuEvent::RUN_END, testing::_))
+            .Times(1);
 }
