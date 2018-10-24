43d3acf7c882e72f7cc6e6a873f419461479e0b2
László <laci.dj@gmail.com>
2018-10-24 21:07:32 +0200

    mock handler classes

--- sudooku_tests/controller_tests/helper_classes/mockInputHandler.h
+++ sudooku_tests/controller_tests/helper_classes/mockInputHandler.h
@@ -2,8 +2,8 @@
 // Created by Borbély László on 2018. 10. 21..
 //
 
-#ifndef SUDOOKU_MOCKMATRIX_H
-#define SUDOOKU_MOCKMATRIX_H
+#ifndef SUDOOKU_MOCKINPUTHANDLER_H
+#define SUDOOKU_MOCKINPUTHANDLER_H
 
 #include <handlers/input_handlers/inputHandler.h>
 #include "gmock/gmock.h"
@@ -13,4 +13,4 @@ public:
     MOCK_METHOD0(readInput, Matrix());
 };
 
-#endif //SUDOOKU_MOCKMATRIX_H
+#endif //SUDOOKU_MOCKINPUTHANDLER_H
