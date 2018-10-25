f2af547841eef5f684549ec865879f8d81523cfe
László <laci.dj@gmail.com>
2018-10-25 15:29:52 +0200

    core exception defined

---
+++ sudooku_core/exceptions/noStrategiesException.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Borbély László on 2018. 10. 25..
+//
+
+#include "noStrategiesException.h"
+
+const char *NoStrategiesException::what() const noexcept {
+    return "No strategies specified!\n";
+}
+
