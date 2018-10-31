c23248d0b49588a793b3857814ce4dd8311f02f2
László <laci.dj@gmail.com>
2018-10-31 15:55:48 +0100

    IOHandler test comments

--- sudooku_tests/controller_tests/fileInputHandlerTest.cpp
+++ sudooku_tests/controller_tests/fileInputHandlerTest.cpp
@@ -6,20 +6,39 @@
 #include <gmock/gmock.h>
 #include <sudooku_controller/handlers/input_handlers/fileInputHandler.h>
 
+/**
+ * FileInputHandler test fixture class
+ */
 class FileInputHandlerTests : public ::testing::Test {
 protected:
     InputHandler *inputHandler{};
 
+    /**
+     * Setup method running before the execution of each test case
+     */
     void SetUp() override {
+        /* Initialize FileInputHandler object */
         inputHandler = new FileInputHandler{{false}, "test1.mat"};
     }
 
+    /**
+     * Teardown method running after the execution of each test case
+     */
     void TearDown() override {
+        /* Free InputHandler object to prevent memory leak */
         delete inputHandler;
     }
 };
 
+/**
+ * Test case to check whether the object is able to read a Matrix from the input
+ */
 TEST_F(FileInputHandlerTests, matrix_read_test) {
+    /* Reading input file */
     auto matrix = inputHandler->readInput();
+
+    /* Check whether the dimension of the read Matrix is correct */
     ASSERT_EQ(9, matrix.getDimension());
+
+    /* TODO: probably not just the dimension but some values should also be checked */
 }
--- sudooku_tests/controller_tests/fileOutputHandlerTest.cpp
+++ sudooku_tests/controller_tests/fileOutputHandlerTest.cpp
@@ -10,46 +10,82 @@
 #include <sudooku_controller/exceptions/noMatrixProvidedException.h>
 #include <sudooku_controller/exceptions/wrongEventTypeException.h>
 
+/**
+ * FileOutputHandler test fixture class
+ */
 class FileOutputHandlerTests : public ::testing::Test {
 protected:
     OutputHandler *outputHandler{};
 
+    /**
+     * Setup method running before the execution of each test case
+     */
     void SetUp() override {
+        /* Initialize FikeOutoutHandler object */
         outputHandler = new FileOutputHandler{"solution1.mat"};
     }
 
+    /**
+     * Teardown method running after the execution of each test case
+     */
     void TearDown() override {
+        /* Free OutputHandler object to prevent memory leak */
         delete outputHandler;
     }
 };
 
+/**
+ * Test case to check whether Matrix is sent to the OutputHandler
+ */
 TEST_F(FileOutputHandlerTests, matrix_write_test) {
+    /* Initialize Matrix object with only one fixed value */
     Matrix matrix1{9, {{{1, 1}, Field{1}}}};
 
+    /* Notify OutputHandler of execution start */
     outputHandler->notifyEvent(SudookuEvent::RUN_START, nullptr);
+
+    /* Notify OutputHandler of solution found */
     outputHandler->notifyEvent(SudookuEvent::SOLUTION, &matrix1);
+
+    /* Notify OutputHandler of execution end */
     outputHandler->notifyEvent(SudookuEvent::RUN_END, nullptr);
 }
 
+/**
+ * Test case to check whether it's possible to write an undefined Matrix to the output
+ */
 TEST_F(FileOutputHandlerTests, matrix_write_invalid_test) {
+    /* Try sending a solution notification with a nullpointer */
     try {
         outputHandler->notifyEvent(SudookuEvent::SOLUTION, nullptr);
     }
+
+        /* A specific exception should be thrown in this case */
     catch (NoMatrixProvidedException &e) {
         EXPECT_EQ(std::string{"No matrix provided to notify function!"}, e.what());
     }
+
+        /* If for some reason another exception is thrown, the test should fail */
     catch (...) {
         FAIL() << "expected NoMatrixProvidedException";
     }
 }
 
+/**
+ * Test case to check whether it's possible to send a notification with an incorrect event type
+ */
 TEST_F(FileOutputHandlerTests, invalid_event_test) {
+    /* Try sending a notification with the event type of '-1' */
     try {
         outputHandler->notifyEvent(static_cast<SudookuEvent>(-1), nullptr);
     }
+
+        /* A specific exception should be thrown in this case */
     catch (WrongEventTypeException &e) {
         EXPECT_EQ(std::string{"Undefined SudookuEvent!"}, e.what());
     }
+
+        /* If for some reason another exception is thrown, the test should fail */
     catch (...) {
         FAIL() << "expected WrongEventTypeException";
     }
