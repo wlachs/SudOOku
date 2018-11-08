1ec8c04f3420106e399f54bcafd220858f028c2e
László <laci.dj@gmail.com>
2018-11-08 15:28:45 +0100

    ReadMatrixFromFile implementation of SudookuReader

---
+++ sudooku_src/sudooku_visual/read_matrix_from_file/readMatrixFromFile.h
@@ -0,0 +1,29 @@
+//
+// Created by Borbély László on 2018. 11. 01..
+//
+
+#ifndef SUDOOKU_READMATRIXFROMFILE_H
+#define SUDOOKU_READMATRIXFROMFILE_H
+
+#include <fstream>
+#include <sudooku_visual/sudookuReader.h>
+
+class ReadMatrixFromFile : SudookuReader {
+private:
+    const char SEPARATOR = ';';
+    unsigned short int rowIndex = 1;
+    std::ifstream &inputFile;
+    std::vector<Matrix> inputs;
+
+    void readPuzzles();
+
+public:
+    explicit ReadMatrixFromFile(std::ifstream &);
+
+    ~ReadMatrixFromFile() override = default;
+
+    std::vector<Matrix> readAll() override;
+};
+
+
+#endif //SUDOOKU_READMATRIXFROMFILE_H
