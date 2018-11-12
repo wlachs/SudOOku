8e2a389187f85718e4a860f2ee6ddcf415d25c05
László <laci.dj@gmail.com>
2018-11-12 15:51:11 +0100

    create PrintMatrixToFile test file

---
+++ sudooku_tests/visual_tests/printMatrixToFileTest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Borbély László on 2018. 11. 12..
+//
+
+#include <gtest/gtest.h>
+
+/**
+ * ForkHelper test fixture class
+ */
+class PrintMatrixToFileTests : public ::testing::Test {
+protected:
+
+    /**
+     * Setup method running before the execution of each test case
+     */
+    void SetUp() override {
+
+    }
+
+    /**
+     * Teardown method running after the execution of each test case
+     */
+    void TearDown() override {
+
+    }
+};
